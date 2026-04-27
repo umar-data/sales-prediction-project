b4d8dab68419b1ff78947ef0343f48bac79f9bcd
umar-data <datascientist.7.4.15@gmail.com>
2026-04-27 21:57:21 +0530

    Enhance sales prediction app with UI and plotting
    Added data visualization and improved user interface.

--- app.py
+++ app.py
@@ -1,27 +1,56 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-st.title("📊 Sales Prediction App")
+# Page config
+st.set_page_config(page_title="Sales Predictor", layout="centered")
+
+# Title
+st.title("📊 AI-Based Sales Prediction System")
+st.write("This app predicts future sales using Linear Regression.")
+
 
 # Load data
 df = pd.read_csv("data.csv")
 
-st.subheader("📂 Dataset")
-st.write(df)
+# Show dataset
+with st.expander("📂 View Dataset"):
+    st.write(df)
 
-# Train model
+# Prepare data
 X = df[["Month"]]
 y = df["Sales"]
 
+# Train model
 model = LinearRegression()
 model.fit(X, y)
 
 # User input
 st.subheader("🔮 Predict Future Sales")
-month = st.number_input("Enter future month", min_value=1, step=1)
+month = st.number_input("Enter Month", min_value=1, step=1)
 
+#warning
+if month > 24:
+    st.warning("⚠️ Prediction may be less accurate for large months")
+
+# Prediction
 if st.button("Predict"):
     prediction = model.predict([[month]])
-    st.success(f"Predicted Sales for Month {month}: {int(prediction[0])}")
+    
+    st.success(f"💰 Expected Sales for Month {month}: ₹ {int(prediction[0])}")
+    st.write("Made by Umar Shaikh 🚀")
+
+    # Plot graph
+    fig, ax = plt.subplots()
+    ax.scatter(X, y, label="Actual Data")
+    ax.plot(X, model.predict(X), label="Trend Line")
+    ax.scatter(month, prediction, color="red", label="Prediction")
+
+    ax.set_xlabel("Month")
+    ax.set_ylabel("Sales")
+    ax.legend()
+
+    st.pyplot(fig)
+st.markdown("---")
